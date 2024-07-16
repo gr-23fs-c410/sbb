6ac5e44c5c64b34226a42dbc50c0bcbcc82d75cf
shinis(green) <actionshin@gmail.com>
2024-07-16 10:49:40 +0900

    3-05 스프링 시큐리티 적용

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -2,9 +2,12 @@ package com.mysite.sbb.question;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+
 public interface QuestionService {
 	
 	public List<Question> getList();
 	public Question getQuestion(Integer id);
 	public void create(String subject, String content);
+	public Page<Question> getList(int page);
 }
--- src/main/java/com/mysite/sbb/question/impl/QuestionServiceImpl.java
+++ src/main/java/com/mysite/sbb/question/impl/QuestionServiceImpl.java
@@ -1,9 +1,14 @@
 package com.mysite.sbb.question.impl;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.mysite.sbb.DataNotFoundException;
@@ -42,6 +47,14 @@ public class QuestionServiceImpl implements QuestionService {
 			throw new DataNotFoundException("질문을 찾을 수 없습니다.");
 		}
 	}
+
+	@Override
+	public Page<Question> getList(int page) {
+		List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.questionRepository.findAll(pageable);
+    }
 	
 
 }
