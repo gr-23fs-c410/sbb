b74d470cf2bf62f5f9696a7c0d9b8cf73be7803e
shinis(green) <actionshin@gmail.com>
2024-07-17 13:14:16 +0900

    3-09 수정과 삭제 기능 추가

--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import org.springframework.data.domain.Page;
 
+import com.mysite.sbb.user.SiteUser;
+
 public interface QuestionService {
 	
 	public List<Question> getList();
 	public Question getQuestion(Integer id);
-	public void create(String subject, String content);
+	public void create(String subject, String content, SiteUser user);
 	public Page<Question> getList(int page);
+	public void modify(Question question, String subject, String content);
+	public void delete(Question question);
 }
--- src/main/java/com/mysite/sbb/question/impl/QuestionServiceImpl.java
+++ src/main/java/com/mysite/sbb/question/impl/QuestionServiceImpl.java
@@ -15,6 +15,7 @@ import com.mysite.sbb.DataNotFoundException;
 import com.mysite.sbb.question.Question;
 import com.mysite.sbb.question.QuestionRepository;
 import com.mysite.sbb.question.QuestionService;
+import com.mysite.sbb.user.SiteUser;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,11 +24,25 @@ import lombok.RequiredArgsConstructor;
 public class QuestionServiceImpl implements QuestionService {
 	
 	@Override
-	public void create(String subject, String content) {
+	public void delete(Question question) {
+		 this.questionRepository.delete(question);
+	}
+
+	@Override
+	public void modify(Question question, String subject, String content) {
+		question.setSubject(subject);
+        question.setContent(content);
+        question.setModifyDate(LocalDateTime.now());
+        this.questionRepository.save(question);
+	}
+
+	@Override
+	public void create(String subject, String content, SiteUser user) {
 		Question q = new Question();
         q.setSubject(subject);
         q.setContent(content);
         q.setCreateDate(LocalDateTime.now());
+        q.setAuthor(user);
         this.questionRepository.save(q);
 	}
 
